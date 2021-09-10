3af4e970ace240ea80f7fa09ef3a1a21f8110c00
José Andrés Zamora Miranda <43786193+jazamora4@users.noreply.github.com>
2021-09-10 18:56:04 -0500

    Documental inicial

---
+++ Documental/src/main/java/ec/edu/espe/distribuidas/documental/DocumentalApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.documental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentalApplication.class, args);
+    }
+
+}
---
+++ Documental/src/test/java/ec/edu/espe/distribuidas/documental/DocumentalApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.documental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
